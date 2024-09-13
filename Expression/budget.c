6466057c9342f9cccda4b02ef7bd67a23f0c4ccd
AdeleBel <adele.robison@ucas-edu.net>
2024-09-13 19:14:48 +0000

    Finished project

---
+++ Expression/budget.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+main(void){
+    float income, rent, utilities, grocieries, transportantion, savings, spend;
+    float prent, putilities, pgrocieries, ptransportantion, psavings 
+    printf("This is a budget calculator. \n How much do you make a month?\n")
+    scanf("%f", &income);
+    printf("how much do your utilities cost\n")
+    scanf("%f", &utilities cost?\n");
+     printf("how much do your grocieries cost\n")
+    scanf("%f", &grocieries cost?\n");
+    printf("how much does your transportation cost?\n");
+    scanf("%f", &transportantion)
+    savings = income * .2;
+    expenses = rent + utilities + grocieries + transportantion
+    spend = income - expenses - savings;
+    printf("you make %.2f\n", income);
+    printf("Your expenses are %.2f\n", expenses);
+    printf()
+    prent = rent/income * 100;
+    putilities = utilities/income * 100;
+    pgrocieries = grocieries/income * 100;
+    ptransportantion = transportantion/income * 100;
+    psavings = savings/income * 100;
+    printf("Your rent is %d%%" of your income.", (int) prent)
+    return 0;
+}    
