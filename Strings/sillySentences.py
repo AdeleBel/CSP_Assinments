1a3cdf66723a46f37c16da57a8b06c3308d49847
AdeleBel <adele.robison@ucas-edu.net>
2024-10-01 19:25:04 +0000

    fixed

--- Strings/sillySentences.py
+++ Strings/sillySentences.py
@@ -1,7 +1,4 @@
 animal = input("Name an animal: ")
 place = input("Name a place: ")
 verb = input("Name a past tense verb: ")
-print("The", animal)
-print(" had a party at ", place)
-print(" and ", verb)
-print(" with the cat.")
+print("The", animal, " had a party at ", place, " and ", verb, " with the cat.")
