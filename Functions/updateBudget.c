3c36e472b80071a05ff4946045cfdc73a0a84a55
AdeleBel <adele.robison@ucas-edu.net>
2024-10-03 19:39:03 +0000

    Finished and corrected

--- Functions/updateBudget.c
+++ Functions/updateBudget.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
+float income, rent, utilities, groceries, transportation, savings, spent, expenses;
 
-float income, rent, utilities, groceries, transportation, savings, expenses, spend;
 float input(char type[], float var){
-    printf("Monthly %s: \n", type);
-    scanf("%.f", &var);
+    printf("monthly %s:\n", type);
+    scanf("%f", &var);
     return var;
 }
 
+void percent(char type[], int ammount){
+    int per = ammount/income * 100;
 
-
-void percent(char type[], int amount){
-    int per = amount/income *100;
-
-    printf("Your %s is %d%% of your income.\n", (int) "rent" , type, per);
+    printf("your %s is %d%% of your income.\n", type, per);
 }
-
-
+    
 int main(void){
-    printf("This is a budget calculator.\n How much do you make a month?\n");
-    input("income", income);
-    input("rent", rent);
-    input("utilities", utilities);
-    input("grocieries", groceries);
-    input("transpotation", transportation);
+    
+    printf("this is a budget calculator.\n");
+    income = input("income", income);
+    rent = input("rent", rent);
+    utilities = input("utilities", utilities);
+    groceries = input("groceries", groceries);
+    transportation = input("transportation", transportation);
+    
+    savings = income *.2;
+    expenses = rent + utilities + groceries + transportation;
+    spent = income - expenses - savings;
+    
+    printf("you make %.2f\n", income);
+    printf("your expenses are $%.2f\n", expenses);
+    printf("your savings are $%.2f\n", savings);
+    printf("your spending money is $%.2f\n", spent);
 
-    savings = income * .2;
-    expenses = rent + utilities + groceries + transportation + savings;
-    spend = income - expenses - savings;
-    printf("You make $%.2f\n", income);
-    printf("You make $%.2f\n", expenses);
-    printf("You make $%.2f\n", savings);
-    printf("You spending money is $%.2f\n", spend);
     percent("rent", rent);
     percent("utilities", utilities);
     percent("groceries", groceries);
     percent("transportation", transportation);
-    percent("savings", savings);
-
-    
-  return 0;
+    percent("savings", savings); 
+    percent("expenses", expenses);
+    percent("spending", spent);   
+    return 0;
 }
